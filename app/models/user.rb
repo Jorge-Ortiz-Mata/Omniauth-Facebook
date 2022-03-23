57b20723d9585a0dcda2972055d68bff0b4789d1
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-23 13:24:43 -0600

    Second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[facebook]
+
+  def self.from_omniauth(auth)
+    name_split = auth.info.name.split(" ")
+    user = User.where(email: auth.info.email).first
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: '123456')
+    user
+  end
 end
